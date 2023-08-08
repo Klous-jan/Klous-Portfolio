ddcc3bae21aad75f6cfbd7796ea475ceb44b0b11
Klous-jan <razer.nodder@gmail.com>
2023-08-08 16:19:34 -0700

    Updated portfolio w/ playground
    playground included

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,43 +1,3 @@
-// let activeIndex = 0;
-
-// const groups = document.getElementsByClassName("card-grp");
-
-// const handleLoveClick = () => {
-// 	// bump to active index
-// 	const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
-// 	const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
-// 		nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
-
-// 	// Active group becomes after
-// 	currentGroup.dataset.status = "after";
-
-// 	// Next group becomes active
-// 	nextGroup.dataset.status = "becoming-active-from-before";
-
-// 	setTimeout(() => {
-// 		nextGroup.dataset.status = "active";
-// 		activeIndex = nextIndex;
-// 	});
-// };
-
-// const handleHateClick = () => {
-// 	// bump to active index
-// 	const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
-// 	const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
-// 		nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
-
-// 	// Active group becomes after
-// 	currentGroup.dataset.status = "before";
-
-// 	// Next group becomes active
-// 	nextGroup.dataset.status = "becoming-active-from-after";
-
-// 	setTimeout(() => {
-// 		nextGroup.dataset.status = "active";
-// 		activeIndex = nextIndex;
-// 	});
-// };
-
 function flipCard() {
 	var card = document.getElementById("card-flip");
 	card.addEventListener("click", flipCard);
@@ -46,72 +6,104 @@ function flipCard() {
 
 // ===========================================================
 
+var cgi = document.getElementById("cgi");
+var cgii = document.getElementById("cgii");
+var cgiii = document.getElementById("cgiii");
+var cgiv = document.getElementById("cgiv");
+var cgv = document.getElementById("cgv");
+
 var carousel = document.querySelector(".carousel");
 var cellCount = 5;
 var selectedIndex = 0;
-var group = document.getElementById("cgz");
-var activeIndex = group.getAttribute("data-index");
-var indexInc = activeIndex;
-console.log(indexInc);
+// var group = document.getElementById("carousel-top");
+// var activeIndex = group.getAttribute("loc");
+// var indexInc = activeIndex;
+// var clickCounter = 0;
+var array = [1, 2, 3, 4, 5];
 
 function rotateCarousel() {
 	var angle = (selectedIndex / cellCount) * -360;
 	carousel.style.transform = "translateZ(-288px) rotateY(" + angle + "deg)";
-}
-
-function activeCheck() {
-	if (activeIndex == 0) {
-		group.setAttribute("data-index", "4");
-		group.setAttribute("data-status", "unknown");
-	} else if (activeIndex == 1) {
-		group.setAttribute("data-index", "0");
-		group.setAttribute("data-status", "active");
-	} else if (activeIndex == 2) {
-		group.setAttribute("data-index", "1");
-		group.setAttribute("data-status", "unknown");
-	} else if (activeIndex == 3) {
-		group.setAttribute("data-index", "2");
-		group.setAttribute("data-status", "unknown");
-	} else if (activeIndex == 4) {
-		group.setAttribute("data-index", "3");
-		group.setAttribute("data-status", "unknown");
+	selectedIndex = selectedIndex % array.length;
+	cgi.setAttribute("loc", array[selectedIndex]);
+	cgii.setAttribute("loc", array[selectedIndex] - 1);
+	cgiii.setAttribute("loc", array[selectedIndex] - 2);
+	cgiv.setAttribute("loc", array[selectedIndex] - 3);
+	cgv.setAttribute("loc", array[selectedIndex] - 4);
+	if (cgi.getAttribute("loc") == -1 || cgi.getAttribute("loc") == 3 || cgi.getAttribute("loc") == 4 || cgi.getAttribute("loc") == -2) {
+		cgi.setAttribute("hidden", "");
+	} else {
+		cgi.removeAttribute("hidden");
 	}
+	if (cgii.getAttribute("loc") == -1 || cgii.getAttribute("loc") == 3 || cgii.getAttribute("loc") == 4 || cgii.getAttribute("loc") == -2) {
+		cgii.setAttribute("hidden", "");
+	} else {
+		cgii.removeAttribute("hidden");
+	}
+	if (cgiii.getAttribute("loc") == -1 || cgiii.getAttribute("loc") == 3 || cgiii.getAttribute("loc") == 4 || cgiii.getAttribute("loc") == -2) {
+		cgiii.setAttribute("hidden", "");
+	} else {
+		cgiii.removeAttribute("hidden");
+	}
+	if (cgiv.getAttribute("loc") == -1 || cgiv.getAttribute("loc") == 3 || cgiv.getAttribute("loc") == 4 || cgiv.getAttribute("loc") == -2) {
+		cgiv.setAttribute("hidden", "");
+	} else {
+		cgiv.removeAttribute("hidden");
+	}
+	if (cgv.getAttribute("loc") == -1 || cgv.getAttribute("loc") == 3 || cgv.getAttribute("loc") == 4 || cgv.getAttribute("loc") == -2) {
+		cgv.setAttribute("hidden", "");
+	} else {
+		cgv.removeAttribute("hidden");
+	}
+
+	// 012 210 210 521 10-3  -3 0 2 5
+
+	// if (cgi.getAttribute("loc") !== 1) {
+	// 	cgi.setAttribute("opacity", "0.1");
+	// } else {
+	// 	cgi.setAttribute("opacity", "1");
+	// }
+	// if (cgii.getAttribute("loc") !== 1) {
+	// 	cgii.setAttribute("opacity", "0.1");
+	// } else {
+	// 	cgii.setAttribute("opacity", "1");
+	// }
+	// if (cgiii.getAttribute("loc") !== 1) {
+	// 	cgiii.setAttribute("opacity", "0.1");
+	// } else {
+	// 	cgiii.setAttribute("opacity", "1");
+	// }
+	// if (cgiv.getAttribute("loc") !== 1) {
+	// 	cgiv.setAttribute("opacity", "0.1");
+	// } else {
+	// 	cgiv.setAttribute("opacity", "1");
+	// }
+	// if (cgv.getAttribute("loc") !== 1) {
+	// 	cgv.setAttribute("opacity", "0.1");
+	// } else {
+	// 	cgv.setAttribute("opacity", "1");
+	// }
+	// console.log(carousel.style.transform);
+	// console.log("Current Item:", array[selectedIndex]);
 }
 
 // previous-button || LEFT BUTTON || -1
 var prevButton = document.querySelector(".previous-button");
 prevButton.addEventListener("click", function () {
 	selectedIndex--;
-	activeCheck();
+	if (selectedIndex < 0) {
+		selectedIndex = array.length - 1;
+	}
 	rotateCarousel();
-	console.log("current index: " + indexInc);
+	console.log(carousel.style.transform);
+	console.log("Current Item:", array[selectedIndex]);
 });
 
 // next-button || RIGHT BUTTON || +1
 var nextButton = document.querySelector(".next-button");
 nextButton.addEventListener("click", function () {
 	selectedIndex++;
-	// activeIndex (0) + 1 less than or equal to cellCount.length (5) - 1
-	// const nextIndex = activeIndex + 1 >= cellCount.length ? 0 : activeIndex + 1;
-	// const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
-	// 	nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
-
-	// currentGroup.dataset.status = "before";
-	// nextGroup.dataset.status = "becoming-active-from-after";
-	// nextGroup.dataset.status = "active";
 	rotateCarousel();
-
-	// const nextIndex = activeIndex + 1 <= cellCount.length - 1 ? activeIndex - 1 : 0;
-	// const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
-	// 	nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
-
-	// // Active group becomes after
-	// currentGroup.dataset.status = "after";
-
-	// // Next group becomes active
-	// nextGroup.dataset.status = "becoming-active-from-before";
-	// setTimeout(() => {
-	// 	nextGroup.dataset.status = "unknown";
-	// 	activeIndex = nextIndex;
-	// });
+	console.log(carousel.style.transform);
+	console.log("Current Item:", array[selectedIndex]);
 });
